1db04522a83f19ab0016098583ad5a347ab35508
Naihua Chen <naihua.chen@mail.goldennet.com.tw>
2023-11-24 11:35:09 +0800

    [Update -- set fb mes script]

--- pages/_app.js
+++ pages/_app.js
@@ -17,6 +17,32 @@ export default function MyApp({ Component, pageProps }) {
     }
     return <>
         <Head><link rel="icon" href="/favicon.ico" /></Head>
+    <div id="fb-root"></div>
+
+    <div id="fb-customer-chat" class="fb-customerchat">
+    </div>
+
+    <Script>
+      {`var chatbox = document.getElementById('fb-customer-chat');
+      chatbox.setAttribute("page_id", "116435126424428");
+      chatbox.setAttribute("attribution", "biz_inbox");`}
+    </Script>
+    <Script>
+      {`window.fbAsyncInit = function() {
+        FB.init({
+          xfbml            : true,
+          version          : 'v18.0'
+        });
+      };
+
+      (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = 'https://connect.facebook.net/zh_TW/sdk/xfbml.customerchat.js';
+        fjs.parentNode.insertBefore(js, fjs);
+      }(document, 'script', 'facebook-jssdk'));`}
+    </Script>
         <Component {...pageProps} />
     </>
 }
